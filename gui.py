9b0baf6b49a299869c1fd67bd8fcb31346ce546e
Brian Breedlove <brianbreedlove0013@gmail.com>
2023-12-26 09:00:35 -0700

    todo-app, add edit and add button to gui

--- gui.py
+++ gui.py
@@ -5,8 +5,11 @@ import PySimpleGUI as pGui
 label = pGui.Text("Type a todo")
 input_box = pGui.InputText(tooltip="Enter to do", key = "todo")
 add_button = pGui.Button("Add")
+list_box = pGui.Listbox(values=functions.get_todos(), key="todos", enable_events=True, size=[45, 10])
+edit_button = pGui.Button("Edit")
+
 window = pGui.Window("My todo app",
-                     layout=[[label], [input_box, add_button]],
+                     layout=[[label], [input_box, add_button], [list_box, edit_button]],
                      font=("Helvetica", 20))
 while True:
     event, values = window.read()
@@ -16,6 +19,17 @@ while True:
             todos = functions.get_todos()
             todos.append(values['todo'] + "\n")
             functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "Edit":
+            edit_todo = values["todos"][0]
+            new_todo = values["todo"]
+            todos = functions.get_todos()
+            index = todos.index(edit_todo)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+        case "todos":
+            window[todos].update(value=values['todos'][0])
         case pGui.WIN_CLOSED:
             break
 window.close()
