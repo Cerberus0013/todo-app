efbe34ad2fa7d31ad409274bb8a59aa077beb5b8
Brian Breedlove <brianbreedlove0013@gmail.com>
2023-12-26 08:29:20 -0700

    todo-app, and file compressor

--- gui.py
+++ gui.py
@@ -3,8 +3,19 @@ import functions
 import PySimpleGUI as pGui
 
 label = pGui.Text("Type a todo")
-input_box = pGui.InputText(tooltip="Enter to do")
+input_box = pGui.InputText(tooltip="Enter to do", key = "todo")
 add_button = pGui.Button("Add")
-window = pGui.Window("My todo app", layout=[[label], [input_box, add_button]])
-window.read()
+window = pGui.Window("My todo app",
+                     layout=[[label], [input_box, add_button]],
+                     font=("Helvetica", 20))
+while True:
+    event, values = window.read()
+    print(event)
+    match event:
+        case "Add":
+            todos = functions.get_todos()
+            todos.append(values['todo'] + "\n")
+            functions.write_todos(todos)
+        case pGui.WIN_CLOSED:
+            break
 window.close()
