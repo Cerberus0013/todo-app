a7238948a48c32d2fb460e8e8721dc8a58fba33f
Brian Breedlove <brianbreedlove0013@gmail.com>
2023-12-27 05:03:58 -0700

    complete and exit buttons

--- file_compression_app/gui.py
+++ file_compression_app/gui.py
@@ -1,15 +1,27 @@
 import PySimpleGUI as pGui
+import zip_creator as make_archive
 
 label1 = pGui.Text("Select file to compress:")
 input1 = pGui.Input()
-choose_button1 = pGui.FilesBrowse("Choose")
+choose_button1 = pGui.FilesBrowse("Choose", key="files")
 
 label2 = pGui.Text("Select destination folder:")
 input2 = pGui.Input()
-choose_button2 = pGui.FolderBrowse("Choose")
+choose_button2 = pGui.FolderBrowse("Choose", key="folder")
 
-window = pGui.Window("File Compressor", layout=[[label1, input1, choose_button1], [label2, input2, choose_button2]])
+compress_button = pGui.Button("Compress")
+output_label = pGui.Text(key="output")
 
+window = pGui.Window("File Compressor", layout=[[label1, input1, choose_button1],
+                                                [label2, input2, choose_button2],
+                                                [compress_button, output_label]])
 
-window.read()
+while True:
+    event, values = window.read()
+    print(event, values)
+    filepath = values["files"].split(";")
+    folder = values("folder")
+    make_archive(filepath, folder)
+    window["output"].update(value="Compression Completed")
+
 window.close()
--- gui.py
+++ gui.py
@@ -3,13 +3,17 @@ import functions
 import PySimpleGUI as pGui
 
 label = pGui.Text("Type a todo")
-input_box = pGui.InputText(tooltip="Enter to do", key = "todo")
+input_box = pGui.InputText(tooltip="Enter to do", key="todo")
 add_button = pGui.Button("Add")
 list_box = pGui.Listbox(values=functions.get_todos(), key="todos", enable_events=True, size=[45, 10])
 edit_button = pGui.Button("Edit")
+complete_button = pGui.Button("Complete")
+exit_button = pGui.Button("Exit")
 
 window = pGui.Window("My todo app",
-                     layout=[[label], [input_box, add_button], [list_box, edit_button]],
+                     layout=[[label],
+                        [input_box, add_button],
+                        [list_box, edit_button, complete_button], [exit_button]],
                      font=("Helvetica", 20))
 while True:
     event, values = window.read()
@@ -30,6 +34,15 @@ while True:
             window['todos'].update(values=todos)
         case "todos":
             window[todos].update(value=values['todos'][0])
+        case "Complete":
+            todo_to_complete = values["todos"][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window["todos"].update(value=todos)
+            window["todo"].update(value="")
+        case "Exit":
+            break
         case pGui.WIN_CLOSED:
             break
 window.close()
